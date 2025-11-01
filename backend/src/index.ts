f8c47ec8638e8a5ee84e81828821addf75142d79
Rakshit Hooda <rakshit.official703@gmail.com>
2025-11-01 17:43:18 +0530

    Setup express server and PORT for it

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
+import uploadRouter from "./routes/upload.js";  // Coz we're using "type: module"
+
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 4000;
